8bb8d2f1febfc34a05009eaf6a11c12c6d59ea3d
Eleven <idiotalex@163.com>
2025-09-28 01:46:38 +0800

    refactor: 简化远程图像主机名配置，使用通配符替代具体主机名

--- next.config.ts
+++ next.config.ts
@@ -12,35 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "yt3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "yt3.ggpht.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "designmodo.com",
-      },
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_UPLOADTHING_URL!,
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "nexty.dev",
+        hostname: "*",
       },
     ],
   },
